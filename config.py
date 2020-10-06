3d2083d300c6fb93899a026bbfa56a7fc3162161
Nick <nicholas.g1993@hotmail.com>
2020-10-06 23:08:58 +0300

    fixing migrate issue in Heroku

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Database connection string
-SQLALCHEMY_DATABASE_URI = 'postgres://dnfqslluqepqxp:553bd7a9ff3b8ce448cba49b96bd0aac83441d97508e3a6807e9204a66e26787@ec2-54-157-234-29.compute-1.amazonaws.com:5432/da939c850dc00q'
+SQLALCHEMY_DATABASE_URI = 'postgres://sskpjelmbyfoez:97ec88947080115361d69e93495c96ff3dd4499babf98dedf31fc044a1c1099f@ec2-52-87-22-151.compute-1.amazonaws.com:5432/d284mn0tbnns06'
 # Supress warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
